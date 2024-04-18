fdfe3d5ef75bf18e6eae2a5eeda729b993e4937b
Sachin Bhawar <95158797+sachinbhawar2014@users.noreply.github.com>
2024-04-18 22:50:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,45 @@
-
 import pygame
 from pygame.locals import *
 import random
 import time
 
+clock = pygame.time.Clock()
 
 BLOCK_SIZE = 40
-BACKGROUND_COLOR = (110, 110, 5)
+BACKGROUND_COLOR = (135, 245, 66)
+BACKGROUND_COLOR_GAME_OVER = (227, 5, 56)
+BORDER_COLOR = (66, 245, 93)
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+X_Blocks = WINDOW_WIDTH // BLOCK_SIZE
+Y_Blocks = WINDOW_HEIGHT // BLOCK_SIZE
+clock = pygame.time.Clock()
 
 class Apple:
     def __init__(self, parent_screen):
         self.parent_screen = parent_screen
         self.image = pygame.image.load("resources/apple.jpg").convert()
-        self.x = 120
-        self.y = 120
+        self.x = 3 * BLOCK_SIZE
+        self.y = 3 * BLOCK_SIZE
 
     def draw(self):
         self.parent_screen.blit(self.image, (self.x, self.y))
         pygame.display.flip()
 
     def move(self):
-        self.x = random.randint(1,24)*BLOCK_SIZE
-        self.y = random.randint(1,19)*BLOCK_SIZE
+        self.x = random.randint(2,X_Blocks-2) * BLOCK_SIZE
+        self.y = random.randint(2,Y_Blocks-2) * BLOCK_SIZE
+        print('3')
 
 class Snake:
     def __init__(self, parent_screen):
         self.parent_screen = parent_screen
         self.image = pygame.image.load("resources/block.jpg").convert()
         self.direction = 'down'
-
         self.length = 1
         self.x = [40]
         self.y = [40]
+        # self.speed = 0.25  # Speed increase factor
 
     def move_left(self):
         self.direction = 'left'
@@ -47,9 +55,10 @@ class Snake:
 
     def walk(self):
         # update body
-        for i in range(self.length-1,0,-1):
-            self.x[i] = self.x[i-1]
-            self.y[i] = self.y[i-1]
+        # time.sleep(self.speed)
+        for i in range(self.length - 1, 0, -1):
+            self.x[i] = self.x[i - 1]
+            self.y[i] = self.y[i - 1]
 
         # update head
         if self.direction == 'left':
@@ -71,22 +80,23 @@ class Snake:
 
     def increase_length(self):
         self.length += 1
-        self.x.append(-1)
-        self.y.append(-1)
+        self.x.append(BLOCK_SIZE)
+        self.y.append(BLOCK_SIZE)
 
 class Game:
     def __init__(self):
         pygame.init()
         pygame.display.set_caption("Sachin Bhawar Snake And Apple Game Project")
-
         pygame.mixer.init()
         self.play_background_music()
-
-        self.surface = pygame.display.set_mode((1000, 800))
+        self.speed = 2
+        self.surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.pause = False
         self.snake = Snake(self.surface)
         self.snake.draw()
         self.apple = Apple(self.surface)
         self.apple.draw()
+        # self.clock = pygame.time.Clock()
 
     def play_background_music(self):
         pygame.mixer.music.load('resources/bg_music_1.mp3')
@@ -111,8 +121,12 @@ class Game:
         return False
 
     def render_background(self):
-        bg = pygame.image.load("resources/background.jpg")
-        self.surface.blit(bg, (0,0))
+        self.surface.fill(BACKGROUND_COLOR)
+        pygame.draw.line(self.surface, BORDER_COLOR, (0, 0), (WINDOW_WIDTH, 0), BLOCK_SIZE)  # Top
+        pygame.draw.line(self.surface, BORDER_COLOR, (0, 0), (0, WINDOW_HEIGHT), BLOCK_SIZE)  # Left
+        pygame.draw.line(self.surface, BORDER_COLOR, (WINDOW_WIDTH-1, 0), (WINDOW_WIDTH-1, WINDOW_HEIGHT), BLOCK_SIZE)  # Right
+        pygame.draw.line(self.surface, BORDER_COLOR, (0, WINDOW_HEIGHT-1), (WINDOW_WIDTH, WINDOW_HEIGHT-1), BLOCK_SIZE)  # Bottom
+
 
     def play(self):
         self.render_background()
@@ -121,25 +135,35 @@ class Game:
         self.display_score()
         pygame.display.flip()
 
-        # snake eating apple scenario
+        # Snake eating apple
         if self.is_collision(self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y):
             self.play_sound("ding")
+            self.speed += 1
             self.snake.increase_length()
             self.apple.move()
+            
+            # Increase the speed of the snake by 5% each time an apple is eaten
+            # SCREEN_REFRESH_RATE *= self.snake.speed_increase
 
-        # snake colliding with itself
+        # Snake colliding with itself
         for i in range(3, self.snake.length):
             if self.is_collision(self.snake.x[0], self.snake.y[0], self.snake.x[i], self.snake.y[i]):
                 self.play_sound('crash')
-                raise "Collision Occurred"
+                raise Exception("Collision Occurred")
+            
+        # Snake crossing boundaries
+        if (self.snake.x[0] >= (WINDOW_WIDTH-BLOCK_SIZE) or self.snake.x[0] < BLOCK_SIZE or self.snake.y[0] >= (WINDOW_HEIGHT-BLOCK_SIZE) or self.snake.y[0] < BLOCK_SIZE):
+            self.play_sound('crash')
+            raise Exception("Collision Occurred")
 
     def display_score(self):
-        font = pygame.font.SysFont('arial',30)
-        score = font.render(f"Score: {self.snake.length}",True,(200,200,200))
-        self.surface.blit(score,(850,10))
+        font = pygame.font.SysFont('arial', 30)
+        score = font.render(f"Score: {self.snake.length}", True, (200, 200, 200))
+        self.surface.blit(score, (850, 10))
 
     def show_game_over(self):
-        self.render_background()
+        # self.render_background()
+        self.surface.fill(BACKGROUND_COLOR_GAME_OVER)
         font = pygame.font.SysFont('arial', 30)
         line1 = font.render(f"Game is over! Your score is {self.snake.length}", True, (255, 255, 255))
         self.surface.blit(line1, (200, 300))
@@ -150,7 +174,7 @@ class Game:
 
     def run(self):
         running = True
-        pause = False
+        self.pause = False
 
         while running:
             for event in pygame.event.get():
@@ -160,9 +184,9 @@ class Game:
 
                     if event.key == K_RETURN:
                         pygame.mixer.music.unpause()
-                        pause = False
+                        self.pause = False
 
-                    if not pause:
+                    if not self.pause:
                         if event.key == K_LEFT:
                             self.snake.move_left()
 
@@ -177,17 +201,19 @@ class Game:
 
                 elif event.type == QUIT:
                     running = False
+
             try:
-
-                if not pause:
+                if not self.pause:
                     self.play()
-
             except Exception as e:
                 self.show_game_over()
-                pause = True
+                self.pause = True
+                self.speed =1
                 self.reset()
 
-            time.sleep(.25)
+
+            clock.tick(self.speed)
+            # time.sleep(0.25)
 
 if __name__ == '__main__':
     game = Game()
